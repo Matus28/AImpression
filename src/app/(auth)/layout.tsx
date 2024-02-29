7f289c33674bf39c920e62f7be47b8c0d3a6c011
Matus Tatarko <matus.tatarko28@gmail.com>
2024-03-01 00:10:29 +0100

    feat: Create main Home route

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const Layout = ({ children }: { children: React.ReactNode}) => {
+ return (
+    <main className='auth'>{children}</main>
+  )
+}
+
+export default Layout
